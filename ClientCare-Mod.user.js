6a6a5b552a34b935ee61e41ff89b20f7d3c9c191
HASDAK Onishim <OHK1@dsone.3ds.com>
2018-09-03 16:49:45 +0800

    Added User Control on Features (v0.3)
    - Variables added for users to enable/disable features

--- ClientCare-Mod.user.js
+++ ClientCare-Mod.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ClientCare-Mod
-// @version      0.2
+// @version      0.3
 // @description  Enhancement mods for ClientCare
 // @author       Oni
 // @match        https://dsxclient.3ds.com/psp/*
@@ -14,13 +14,21 @@
 // @grant        log
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    // Add Style (CSS) to page
-    // Add Bootstrap CSS library to page
-    //var bscss = GM_getResourceText ("bootstrap");
-    //GM_addStyle(bscss);
-    var cccss = GM_getResourceText ("cc_mod_css");
-    GM_addStyle(cccss);
-    console.log('Added CSS');
-})();
+// Add Style (CSS) to page
+// Add Bootstrap CSS library to page
+//var bscss = GM_getResourceText ("bootstrap");
+//GM_addStyle(bscss);
+var cccss = GM_getResourceText ("cc_mod_css");
+GM_addStyle(cccss);
+console.log('Added CSS');
+
+/**--------------------------------------------------
+ * Enable (true) or Disable (false) features
+ */
+
+// 1. 	Shrink conversations in history upon load of SR page [from Phase 1, but opposite]
+var user_comment_mod = true;
+// 2. 	Highlight SUMMARY note
+var user_summary_mod = true;
+// 4.  Hide 'From Submitter' & 'To Submitter' note buttons
+var user_hide_button = true;
--- cc_mod.js
+++ cc_mod.js
@@ -1,11 +1,11 @@
 /**
- * ClientCare Mod v0.2
+ * ClientCare Mod v0.3
  *
  * Features :
- *  1.  
- *  2.  
- *  3.  
- *  4.  
+ *  1.  Shrink conversations in history upon load of SR page [from Phase 1, but opposite]
+ *  2.  Highlight SUMMARY note
+ *  3.  Bigger font size
+ *  4.  Hide 'From Submitter' & 'To Submitter' note buttons
  */
 
 
@@ -28,9 +28,39 @@ var SelectedTab = ""; // default 'Request' tab
 var summary_div = "";
 // --------------------------------------------------
 
+/**--------------------------------------------------
+     * Enable (true) or Disable (false) features
+     * Default = true
+     */
+
+    // 1.   Shrink conversations in history upon load of SR page [from Phase 1, but opposite]
+    var comment_mod = true;
+    // 2.   Highlight SUMMARY note
+    var summary_mod = true;
+    // 3.   Bigger font size
+    var font_mod = true;
+    // 4.  Hide 'From Submitter' & 'To Submitter' note buttons
+    var hide_button = true;
+
+// --------------------------------------------------
+
 $(document).ready(function(e) {
     //console.log('RootNode =', RootNode);
     //console.log('PageInfo =', PageInfo);
+
+    // override user's feature preferences
+    if (typeof user_comment_mod !== 'undefined') {
+      comment_mod = user_comment_mod;
+    }
+    if (typeof user_summary_mod !== 'undefined') {
+      summary_mod = user_summary_mod;
+    }
+    if (typeof user_font_mod !== 'undefined') {
+      font_mod = user_font_mod;
+    }
+    if (typeof user_hide_button !== 'undefined') {
+      hide_button = user_hide_button;
+    }
 });
 
 if (window.top === window.self) { //--- Script is on domain_B.com when/if it is the MAIN PAGE.
@@ -162,11 +192,15 @@ function CustomizePage(parent_node){
     if( SelectedTab == "CRMS_REQUEST_PG2" ){
         var messaging_field = parent_node.find(id_MessagingField);
         messaging_field.css({'background-image':'none', 'background-color':'ghostwhite'});
-        var btn_FromSubmitter = messaging_field.find("div[id$='CRMS_REQ_WRK_CRMS_REQADDINF1']");
-        btn_FromSubmitter.css({'display':'none'});
-        var btn_ToSubmitter = messaging_field.find("div[id$='CRMS_REQ_WRK_CRMS_REQADDINF2']");
-        btn_ToSubmitter.css({'display':'none'});
-        //console.log('Buttons Hidden :', btn_FromSubmitter, btn_ToSubmitter);
+        
+        if(hide_button){
+            console.log('>> DISABLE BUTTON MOD - [', hide_button? 'ON' : 'OFF', '] <<');
+            var btn_FromSubmitter = messaging_field.find("div[id$='CRMS_REQ_WRK_CRMS_REQADDINF1']");
+            btn_FromSubmitter.css({'display':'none'});
+            var btn_ToSubmitter = messaging_field.find("div[id$='CRMS_REQ_WRK_CRMS_REQADDINF2']");
+            btn_ToSubmitter.css({'display':'none'});
+            //console.log('Buttons Hidden :', btn_FromSubmitter, btn_ToSubmitter);
+        }
     }
 }
 
@@ -174,33 +208,48 @@ function CustomizePage(parent_node){
  * Comment Mod
  */
 function Comment_Mod(parent_node){
-    console.log('>> COMMENT MOD <<');
+    if(comment_mod){
+        console.log('>> COMMENT MOD - [', comment_mod ? 'ON' : 'OFF', '] <<');
+    }
+    if(summary_mod){
+        console.log('>> SUMMARY MOD - [', summary_mod ? 'ON' : 'OFF', '] <<');
+    }
+
     var comments = parent_node.find("div[id*='CRMS_REQ_WRK_CRMS_HTMLTEXT']");
     //console.log('comments :', comments);
     for(var i = 0; i < comments.length; i++){
         var comment = $(comments[i]);
-        var comment_topbar = comment.find('span.comment_topbar');
-        console.log('comment topbar', comment_topbar.length);
-        if(!comment_topbar.length){
-            comment.prepend('<span class="comment_topbar" target="' + comment.attr('id')+ '"></span>');
-            comment_topbar = comment.find('span.comment_topbar');
+        if(comment_mod){
+            var comment_topbar = comment.find('span.comment_topbar');
+            //console.log('comment topbar', comment_topbar.length);
+            if(!comment_topbar.length){
+                comment.prepend('<span class="comment_topbar" target="' + comment.attr('id')+ '"></span>');
+                comment_topbar = comment.find('span.comment_topbar');
+            }
+
+            if(comment.height() > 100){
+            
+                comment.addClass('hide_comment');
+                comment_topbar.html('[+]');
+                comment_topbar.click(function(e2){
+                    e2.stopPropagation();
+                    var target = $(this).parent();
+                    if(target.hasClass('hide_comment')){ // expand comment
+                        target.removeClass('hide_comment');
+                        $(this).html('[-]');
+                    }else{                               // collapse comment
+                        target.addClass('hide_comment');
+                        $(this).html('[+]');
+                    }
+                });
+            
+            //comment.css({'font-size' : '9pt'});
+            //console.log('comment ['+i+'] :', comment);
+            }
         }
 
-        if(comment.height() > 100){
-            comment_topbar.html('[+]');
-            comment_topbar.click(function(e2){
-                e2.stopPropagation();
-                var target = $(this).parent();
-                if(target.hasClass('hide_comment')){ // expand comment
-                    target.removeClass('hide_comment');
-                    $(this).html('[-]');
-                }else{                               // collapse comment
-                    target.addClass('hide_comment');
-                    $(this).html('[+]');
-                }
-            });
-
-            // SUMMARY Mod
+        // SUMMARY Mod
+        if(summary_mod){
             var message = comment.find('span[class*="CRMS_TXT"]');
             var firstline = message.text().split('\n')[0];
             //console.log('[', i, '] :', message.text());
@@ -212,9 +261,6 @@ function Comment_Mod(parent_node){
                 message.addClass('summary_note');
                 //scrollTo(summary_div);
             }
-            comment.addClass('hide_comment');
-            comment.css({'font-size' : '9pt'});
-            //console.log('comment ['+i+'] :', comment);
         }
     }
 }
